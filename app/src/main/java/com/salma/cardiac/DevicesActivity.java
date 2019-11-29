36b19491cde9877edefd98564fb2e9f687fe2cd1
ahmed <ahmedmensh8@gmail.com>
2019-11-29 18:21:21 +0200

    paired with device

--- app/src/main/java/com/salma/cardiac/DevicesActivity.java
+++ app/src/main/java/com/salma/cardiac/DevicesActivity.java
@@ -3,8 +3,11 @@ package com.salma.cardiac;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCallback;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -14,6 +17,7 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,16 +27,23 @@ import me.aflak.bluetooth.interfaces.DiscoveryCallback;
 
 public class DevicesActivity extends AppCompatActivity {
 
+    private static final String TAG = "DevicesActivity";
+
     Bluetooth bluetooth;
     private ArrayAdapter<String> scanListAdapter;
     private List<BluetoothDevice> scannedDevices;
+    private List<BluetoothDevice> pairedDevices;
+    private ArrayAdapter<String> pairedListAdapter;
     private boolean scanning = false;
+    private BluetoothGatt bluetoothGatt;
+    private BluetoothGattCallback miBandGattCallBack;
 
 
     TextView state;
     ProgressBar progress;
     Button scanButton;
     ListView deviceList;
+    ListView pairedDeviceList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,7 +55,12 @@ public class DevicesActivity extends AppCompatActivity {
         progress = findViewById(R.id.activity_scan_progress);
         state = findViewById(R.id.activity_scan_state);
         deviceList = findViewById(R.id.activity_scan_list);
+        pairedDeviceList = findViewById(R.id.activity_scan_paired_list);
 
+        // list for paired devices
+        pairedListAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, new ArrayList<String>());
+        pairedDeviceList.setAdapter(pairedListAdapter);
+        pairedDeviceList.setOnItemClickListener(onPairedListItemClick);
 
 
         scanListAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, new ArrayList<String>());
@@ -70,20 +86,6 @@ public class DevicesActivity extends AppCompatActivity {
         });
     }
 
-
-    private void setProgressAndState(String msg, int p) {
-        state.setText(msg);
-        progress.setVisibility(p);
-    }
-
-//    private void displayPairedDevices() {
-//        pairedDevices = bluetooth.getPairedDevices();
-//        for (BluetoothDevice device : pairedDevices) {
-//            pairedListAdapter.add(device.getAddress() + " : " + device.getName());
-//        }
-//    }
-
-
     private AdapterView.OnItemClickListener onPairedListItemClick = new AdapterView.OnItemClickListener() {
         @Override
         public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
@@ -94,6 +96,19 @@ public class DevicesActivity extends AppCompatActivity {
         }
     };
 
+    private void displayPairedDevices(){
+        pairedDevices = bluetooth.getPairedDevices();
+        for(BluetoothDevice device : pairedDevices){
+            pairedListAdapter.add(device.getAddress()+" : "+device.getName());
+        }
+    }
+
+    private void setProgressAndState(String msg, int p) {
+        state.setText(msg);
+        progress.setVisibility(p);
+    }
+
+
     private AdapterView.OnItemClickListener onScanListItemClick = new AdapterView.OnItemClickListener() {
         @Override
         public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
@@ -101,16 +116,36 @@ public class DevicesActivity extends AppCompatActivity {
                 bluetooth.stopScanning();
             }
             setProgressAndState("Pairing...", View.VISIBLE);
-            bluetooth.pair(scannedDevices.get(i));
+            try {
+                if (createBond(scannedDevices.get(i))){
+                    Toast.makeText(DevicesActivity.this,"Paired",Toast.LENGTH_LONG).show();
+                }
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                Log.e(TAG, "onItemClick: "+e.getLocalizedMessage() );
+            }
+
         }
     };
+
+    public boolean createBond(BluetoothDevice btDevice)
+            throws Exception
+    {
+        Class class1 = Class.forName("android.bluetooth.BluetoothDevice");
+        Method createBondMethod = class1.getMethod("createBond");
+        Boolean returnValue = (Boolean) createBondMethod.invoke(btDevice);
+        return returnValue.booleanValue();
+    }
+//
 
     @Override
     protected void onStart() {
         super.onStart();
         bluetooth.onStart();
         if(bluetooth.isEnabled()){
-//            displayPairedDevices();
+            displayPairedDevices();
             scanButton.setEnabled(true);
         } else {
             bluetooth.showEnableDialog(DevicesActivity.this);
@@ -137,7 +172,7 @@ public class DevicesActivity extends AppCompatActivity {
 
         @Override
         public void onBluetoothOn() {
-//            displayPairedDevices();
+            displayPairedDevices();
             scanButton.setEnabled(true);
         }
 
